737e691c32ede4f6fdaad8bbb3e4925769e71455
Emilse Arias <emilsearias@gmail.com>
2023-05-23 10:56:29 -0300

    agregamos el maquetado de los botones

---
+++ src/components/ListaTareas.jsx
@@ -0,0 +1,15 @@
+import { ListGroup } from "react-bootstrap";
+import ItemTarea from "./ItemTarea";
+
+const ListaTareas = () => {
+  return (
+    <ListGroup>
+      <ItemTarea></ItemTarea>
+      <ItemTarea></ItemTarea>
+      <ItemTarea></ItemTarea>
+      <ItemTarea></ItemTarea>
+    </ListGroup>
+  );
+};
+
+export default ListaTareas;
